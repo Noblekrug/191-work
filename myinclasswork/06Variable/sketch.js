e2d85fdc4d8315b3be21e4d85fa7d9fc1b808b39
Conner Kruger-Morrison <conner.kruger-morrison@umontana.edu>
2017-02-13 10:52:40 -0700

    classwork

--- myinclasswork/06Variable/sketch.js
+++ myinclasswork/06Variable/sketch.js
@@ -1,31 +1,17 @@
 function setup() {
-    createCanvas(500, 500);
-    background(255, 100, 200);
+      createCanvas(800,800);
+      background(200);
 
-    translate(250,250);
-    push();
-    fill(0,255,0);
-    ellipse(0, 0, 300, 120);
-    pop();
-
-    push();
-    ellipse(-30,-30,20,20)
-    ellipse(30,-30,20,20)
-
-
-    push();
-    line(-75, 10, 75, 10);
-    pop();
-
-    push();
-    fill(0,255,0);
-    triangle(0,-30,0,10,30,20);
-    pop();
+      translate(400,400)
+      myFirstVar = 70;
+      ellipse90,0,myFirstVar,myFirstVar);
 
 
 
 }
 
 function draw() {
+      myFirstVar = myFirstVar * 0.5;
+      ellipse(0,0,myFirstVar,myFirstVar)
 
 }
