f7f82590e31ac7568806d1e020150c6e279dea09
Conner Kruger-Morrison <conner.kruger-morrison@umontana.edu>
2017-02-15 22:43:45 -0700

    finished sketch

--- Hw-3/sketch.js
+++ Hw-3/sketch.js
@@ -2,10 +2,52 @@ function setup() {
   createCanvas(700,700)
   background(0,0,250)
 
+
+
+  push();
+  fill(245,205,95)
+  ellipse(250,490,50,50)
+  pop();
+
+  push();
+  fill(245,205,95)
+  ellipse(450,490,50,50)
+  pop();
+
+  push();
+  translate(-35, -20)
+  fill(65,45,1)
+  rect(250,300,50,200)
+  pop();
+
+  push();
+  translate(-15,-20)
+  fill(65,45,1)
+  rect(450,300,50,200)
+  pop();
+
+  push();
+  fill(65,41,1)
+  translate(213,205)
+  rect(50,50,175,250,20)
+  pop();
+
   push();
   translate(0, 55)
   fill(245,205,95)
   triangle(350,25,300,200,400,200)
+  pop();
+
+  push();
+  translate(213,430)
+  fill(41,142,196)
+  rect(50,50,175,250)
+  pop();
+
+  push();
+  stroke(0,0,250)
+  strokeWeight(9)
+  line(350,530,350,700)
   pop();
 
   push();
@@ -87,6 +129,8 @@ function setup() {
   push();
   line(390,152,390,173)
   pop();
+
+
 
 
 
