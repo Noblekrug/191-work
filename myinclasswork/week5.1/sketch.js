39550ca981c3880956e186c029ef93ade35e5493
Conner Kruger-Morrison <conner.kruger-morrison@umontana.edu>
2017-03-01 10:56:10 -0700

    class stuff

--- myinclasswork/week5.1/sketch.js
+++ myinclasswork/week5.1/sketch.js
@@ -1,9 +1,20 @@
+  var cnt = 0;
+  var goal = 10;
+
+
 function setup() {
-  
 
 
+    createCanvas(800,600);
+
+    while (cnt <= goal) {
+        cnt++;
+        console.log(cnt);
+    }
 }
 
 function draw() {
+
+
 
 }
