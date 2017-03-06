3f3c6c51bc0b6d15df6bc11cd510472f36755a93
Conner Kruger-Morrison <conner.kruger-morrison@umontana.edu>
2017-03-06 11:14:54 -0700

    in class work

--- myinclasswork/week7.1/sketch.js
+++ myinclasswork/week7.1/sketch.js
@@ -1,7 +1,50 @@
-function setup() {
 
-}
+    person = {
+        location_x: 0,
+        width: 60,
+        height: 120,
+        eye_size: 20,
+        eye_color: 0,
+        color: 0,
+    };
 
-function draw() {
-  
-}
+    function setup() {
+        createCanvas(800,600);
+        background(240);
+
+
+
+        person.location_x = width / 2;
+        person.location_y = height - person.height / 2;
+
+        person.color = color(10,20,230);
+        person.eye_color = color(0,255,0);
+    }
+
+    function draw() {
+
+        push();
+
+        translate(person.location_x, person.location_y)
+
+        fill(person.color)
+
+        rectMode(CENTER)
+        rect(0, 0, person.width, person.height);
+
+        // eyes
+        fill(person.eye_color)
+        ellipse(-person.eye_size*0.8, -(person.height * 0.25), person.eye_size, person.eye_size)
+        ellipse(-person.eye_size*-0.8, -(person.height * 0.25), person.eye_size, person.eye_size)
+
+        fill(0)
+        ellipse(-person.eye_size*0.8, -(person.height * 0.25), person.eye_size*0.3, person.eye_size)
+        ellipse(-person.eye_size*-0.8, -(person.height * 0.25), person.eye_size*0.3, person.eye_size)
+
+
+        pop();
+
+
+
+
+    }
