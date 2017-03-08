03d96c8206e0f4108380902c4381403c88423ab7
Conner Kruger-Morrison <conner.kruger-morrison@umontana.edu>
2017-03-08 11:00:44 -0700

    classwork

--- myinclasswork/week7.2_cont/sketch.js
+++ myinclasswork/week7.2_cont/sketch.js
@@ -1,7 +1,23 @@
+var myArray = [];
+
+var numArray = ["Ha", "Js", ":)"];
+
+var bg_color;
+
+
 function setup() {
+      createCanvas(windowWidth,windowHeight);
+      bg_color = color(230,240,200);
+      background(bg_color);
+
+      textSize(56);
+      text("myArray:" + myArray, 20, 80);
+      text("numArray:" + numArray [2], 20, 200)
+
+
 
 }
 
 function draw() {
-  
+
 }
