8d224dfba5479239d9f1a60d80391659105c5f4c
Conner Kruger-Morrison <conner.kruger-morrison@umontana.edu>
2017-03-08 10:20:06 -0700

    classwork, new directory

--- myinclasswork/week7.2/sketch.js
+++ myinclasswork/week7.2/sketch.js
@@ -1,7 +1,38 @@
 function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background(230, 240,200);
 
 }
 
 function draw() {
-  
+
+    for(var i = 0; i < width/6; i++) {
+
+    }
+
+    drawPerson();
+
+    if(true);
+}
+
+
+function drawPerson(){
+    var pos_x = x;
+    var pos_y = y;
+    var person_width = 80;
+    var person_height = 200;
+
+
+    push();
+
+    translate(pos_x, pos_y);
+
+    rectMode(CENTER);
+    fill(0,10,244);
+    rect(0, 0, person_width * 0.5, person_height);
+
+    ellipse(-person_width * 0.3, -person_height * 0.1, person_width*0.1, person_width*0.1);
+    ellipse(person_width * 0.3, person_height * 0.1, person_width*0.1, person_width*0.1);
+
+    pop();
 }
