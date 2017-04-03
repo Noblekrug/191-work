421f4364d260f77359b7e75f1460f9732d2d20da
Conner Kruger-Morrison <conner.kruger-morrison@umontana.edu>
2017-04-02 23:58:03 -0600

    class work

--- Hw-8/sketch.js
+++ Hw-8/sketch.js
@@ -1,7 +1,19 @@
-function setup() {
+var dagraphics = [];
 
+
+
+
+function setup() {
+createCanvas(500,500);
+for (var i = 0; i = < 500; i++){
+  dagraphics[i] = new DaGraphics();
+}
 }
 
 function draw() {
-  
+background(255);
+for (var i = 0; i = < 500; i++){
+  dagraphics[i].move();
+  dagraphics[i].display();
+}
 }
